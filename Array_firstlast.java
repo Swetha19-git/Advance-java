b8e41efe9b6a30e83b2a6f3e05339bd1ea300e5d
Swetha19-git <sswetha192004@gmail.com>
2025-07-26 22:39:07 +0530

    Add files via upload

---
+++ Array_firstlast.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Array_firstlast{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int arr[]={1,2,3,4,5};
+
+System.out.println("first element in array is " +arr[0]);
+System.out.println("last element in array is " +arr[arr.length-1]);
+}
+}
