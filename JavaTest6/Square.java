a897a9b548d0da4427bd8124c8ce21f155820a14
Swetha19-git <sswetha192004@gmail.com>
2025-09-01 17:49:10 +0530

    Create Square.java

---
+++ JavaTest6/Square.java
@@ -0,0 +1,11 @@
+class Squarenumber{
+ public  int getSquare(int n){
+  return n*n;
+  }
+  public static void main(String args[]){
+   
+   Squarenumber s=new Squarenumber();
+   int num=10;
+   System.out.println(s.getSquare(num));
+   }
+   }
