cbc933ece370c90dd580f113a5d7580c7516bb66
Swetha19-git <sswetha192004@gmail.com>
2025-10-13 18:26:23 +0530

    Create Thread_program.java

---
+++ JavaTest11/Thread_program.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class MyThread extends Thread {
+       @Override
+       public void run() {
+           for (int i = 1; i <= 10; i++) {
+               System.out.println(i);
+           }
+       }
+   }
+
+  
+public class Sample {
+     public static void main(String[] args) {
+           MyThread thread = new MyThread();
+           thread.start();
+     }
+    
+}
